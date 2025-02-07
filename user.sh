72999f8f8bcdca24b32e693c7046951a4aa78c5d
Taraki Bilal <your.email@example.com>
2025-02-07 17:58:46 +0100

    gg

--- user.sh
+++ user.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-echo $HOME
-echo $USER
-echo $PWD
-echo $0
-echo $BASH 
-echo $BASH_VERSION
-name="BILAL TARAKI" #This is just a test.
+#This is just a test.
+hello="hello world"
+echo $hello
+echo our home directory is $HOME
+echo Current username is $USER
+echo our current working directory is $PWD
+echo Name of the script being executed is $0
+echo our shell name is $BASH 
+echo our shell version name is $BASH_VERSION
+name="BILAL TARAKI" 
 echo Hi My Name Is $name
