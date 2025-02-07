26b22478579197a8b10b51a5e2eae77799b3804e
Taraki Bilal <your.email@example.com>
2025-02-07 18:34:18 +0100

    nice

---
+++ array.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "enter names :"
+read -a names # a : array
+echo "names : ${names[0]} , ${names[1]}"
--- password.sh
+++ password.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #test!
-read -p 'usrename :' user_var #-p : To write on the same line
-read -sp 'password :' pass_var #-s :To hide the information entered
+read -p 'usrename :' user_var #p : To write on the same line
+read -sp 'password :' pass_var #s :To hide the information entered
 echo #Start a new line
 echo "usrename : $user_var"
 echo "password : $pass_var"
